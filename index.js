2c74f6d03cc06c0476644a30c7887403c4b50760
Asante <asanterashid.biz@gmail.com>
2021-02-20 23:12:04 -0500

    prompts work but the render function isn't being recognised

--- index.js
+++ index.js
@@ -3,34 +3,74 @@ var inquirer = require('inquirer');
 // fs is a Node standard library package for reading and writing files
 const fs = require('fs');
 // pulls generate markdown functions, notr linking correctly
-const render = require('./generateMarkdown.js')
+const render = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 // basic questions
-const questions = ["Title?", "description", "installation instructions", "usage information", "contribution guidelines", "test instructions" ];
-
-inquirer
-  .prompt([questions
-    /* Pass your questions in here */
-  ])
+function init() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What does your project do?"
+        },
+        {
+            type: "input",
+            name: "installation instructions",
+            message: "How do you install your program?"
+        },
+        {
+            type: "input",
+            name: "usage information",
+            message: "input?"
+        },
+        {
+            type: "input",
+            name: "contribution guidelines",
+            message: "How to contribute?"
+        },
+        {
+            type: "input",
+            name: "test instructions",
+            message: "test instructions"
+        },
+        {
+            type: "checkbox",
+            message: "choose a license",
+            name: "license",
+            choices: [
+                "Apache License 2.0",
+                "GNU General Public License 2.0",
+                "MIT License",
+                "BSD 2-Clause"
+            ]
+        },
+    ])
+}
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     var fileName = "README.md"
 
-    fs.appendFile(fileName, render.generateMarkdown(data) , function(err) {
+    fs.appendFile(fileName, render.generateMarkdown(data), function (err) {
 
         if (err) {
-          console.log(err);
+            console.log(err);
         }
         else {
-          console.log("Commit logged!");
+            console.log("Commit logged!");
         }
-      
-      });
+
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
-init();
+init().then(writeToFile);
